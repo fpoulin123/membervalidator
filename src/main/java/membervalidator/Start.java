09d352f59df091081a4d6e4ec089e58bb33ae1eb
fpoulin123 <fabrice.poulin7@gmail.com>
2025-04-28 10:39:42 -0400

    Add log and message on display panel

--- src/main/java/membervalidator/Start.java
+++ src/main/java/membervalidator/Start.java
@@ -1,8 +1,11 @@
 package membervalidator;
 
+import java.awt.HeadlessException;
+import java.io.IOException;
+
 public class Start {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws HeadlessException, IOException {
 		// TODO Auto-generated method stub
 
 		PrincipalFrame pFrame = new PrincipalFrame();
